733139af5cfd12db9f4aaa6bcb7ff82857d46b12
Thanh-Dat Truong <thanhdattrg@gmail.com>
2022-12-27 22:01:44 -0600

    Clean Comments

--- main.cpp
+++ main.cpp
@@ -20,7 +20,15 @@ struct BoundingBox {
 
 };
 
-const std::vector<std::string> COCO_NAMES = {"person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"};
+const std::vector<std::string> COCO_NAMES = {"person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", 
+                    "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", 
+                    "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", 
+                    "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", 
+                    "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", 
+                    "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", 
+                    "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", 
+                    "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", 
+                    "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"};
 
 class DINO {
     private:
@@ -62,7 +70,6 @@ class DINO {
             auto boxes = tuple[0].toTensor();
             auto scores = tuple[1].toTensor();
             auto classes = tuple[2].toTensor(); 
-            //std::cout << boxes.sizes() << " " << scores.sizes() << " " << classes.sizes() << std::endl;
 
             std::vector<BoundingBox> results;
             for (int i = 0; i < boxes.sizes()[0]; ++i) {
